5b2dd848f963b79847ab1163217dd6da7f4e242d
Sardar-Ali-Murad <mar389786@gmail.com>
2025-01-31 20:03:10 +0500

    changes

---
+++ src/components/landing/colleagues.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import img1 from "../../assets/img-1.svg";
+import img2 from "../../assets/img-2.svg";
+
+const Colleagues = () => {
+  return (
+    <div className="px-[80px] py-[70px] bg-[#083155]">
+      <p className="font-[Raleway] text-[42px] font-bold leading-[49.31px] text-[#FFFFFF]">
+        You have 6 colleagues who have earned over £4000 in the last month for
+        their introductions
+      </p>
+      <div className="flex justidy-between mt-[57px]">
+        <div className="flex gap-[40px]">
+          <img src={img1} className="h-[90px] w-[90px]"/>
+          <div className="flex flex-col gap-[20px]">
+            <p className="font-[Inter] text-[18px] font-bold leading-[21.78px] text-[#FFFFFF]">
+              John Smith - Designer
+            </p>
+            <p className="font-[Inter] text-[18px] italic font-medium leading-[21.78px] text-[#D9D9D9]">
+              “Introducing Nexer to my current employer was super simple and it
+              was great to be able to help both companies... and be paid for it“
+            </p>
+          </div>
+        </div>
+        <div className="flex gap-[40px]">
+          <img src={img2} className="h-[90px] w-[90px]" />
+          <div className="flex flex-col gap-[20px]">
+            <p className="font-[Inter] text-[18px] font-bold leading-[21.78px] text-[#FFFFFF]">
+              Alison Anderson - Tech lead
+            </p>
+            <p className="font-[Inter] text-[18px] italic font-medium leading-[21.78px] text-[#D9D9D9]">
+              “I knew Nexer could help my current employer, so I introduced
+              them. I would have done it anyway, but by using orbit my friend
+              got a discount as well”
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Colleagues;
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/landing/hero";
 import Benefits from "../components/landing/benefits";
+import Colleagues from "../components/landing/colleagues";
 
 const Landing = () => {
   return (
     <div>
       <Hero />
       <Benefits />
+      <Colleagues />
     </div>
   );
 };
